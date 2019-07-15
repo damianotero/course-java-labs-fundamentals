64d5edb40772e59e3ab4a3d8ed41feaaca43e439
damianotero <45541633+damianotero@users.noreply.github.com>
2019-07-15 10:30:39 +0200

    Finished part 1

--- src/main/java/com/codingnomads/part_01/Exercise_08.java
+++ src/main/java/com/codingnomads/part_01/Exercise_08.java
@@ -7,6 +7,23 @@ package com.codingnomads.part_01;
  * and perimeter of a circle with a radius of 3.14
  */
 
-public class Exercise_08 { //TODO THIS EXERCISE  8
+public class Exercise_08 {
+
+
+    public static double getArea(double radius){
+        return Math.PI * (Math.sqrt(radius));
+    }
+
+    public static double getPerimeter(double radius){
+        return  2*Math.PI*radius;
+    }
+
+    public static void main(String[] args) {
+
+        double radius = 3.14;
+
+        System.out.println("Area: "  + getArea(radius));
+        System.out.println("Perimeter: " + getPerimeter(radius));
+    }
 
 }
